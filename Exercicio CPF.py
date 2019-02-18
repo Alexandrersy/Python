5c247095ff0b960a5609767446e60736c3b416b7
Alexandre Silva <47661396+Alexandrersy@users.noreply.github.com>
2019-02-18 09:09:12 -0300

    Add files via upload

---
+++ Exercicio CPF.py
@@ -0,0 +1,10 @@
+cpf=123456789
+nome='Alexandre'
+telefone=98785.4471
+
+print('O meu nome é', nome)
+print(type(nome))
+print('meu telefone é', telefone)
+print(type(telefone))
+print('O meu cpf é' ,cpf)
+print(type(cpf))
