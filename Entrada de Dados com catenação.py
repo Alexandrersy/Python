c8bf88b355928db50f739db19a856ba77a0573e3
Alexandre Silva <47661396+Alexandrersy@users.noreply.github.com>
2019-02-18 11:16:30 -0300

    Add files via upload

---
+++ Entrada de Dados com catenação.py
@@ -0,0 +1,6 @@
+
+nome=str(input('Digite o codigo1: '))
+nome2=str(input('Digite o codigo2: '))
+print('codigo1', nome, '\ncodigo2 ', nome2)
+s= nome+nome2
+print('a soma entre',{},'é',{},'vale',{}, format(nome,nome2,s))
