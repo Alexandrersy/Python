da1449b847410edb9596ad6a67c38dd7203f1702
Alexandre Silva <47661396+Alexandrersy@users.noreply.github.com>
2019-02-27 11:16:31 -0300

    Add files via upload

---
+++ Lista troca de valores.py
@@ -0,0 +1,11 @@
+listaA=[10,20,30,40,50,60,70,80,90]
+listaB=[100,200,300,400,500,600,800,900]
+
+print('Os valores da lista A são: ', listaA)
+print('Os valores da lista B são: ', listaB)
+
+X = listaA
+Y = listaB
+
+print('Os valores trocados da lista A são: ', Y)
+print('Os valores trocados da Lista B são: ', X)
