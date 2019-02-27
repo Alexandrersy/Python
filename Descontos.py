d8bd2e170811adae2aaebe3f9195215f3ce34879
Alexandre Silva <47661396+Alexandrersy@users.noreply.github.com>
2019-02-27 09:01:01 -0300

    Update Descontos.py

--- Descontos.py
+++ Descontos.py
@@ -1,6 +1,6 @@
 
 preco = int(input('digite o preço do produto:'))
-desconto = int(input('digite o valor do desconto'))
+desconto = int(input('digite a porcetagem do desconto(apenas números)'))
 
 soma = (preco -(desconto/100*preco))
 print ('O desconto é =', soma)
